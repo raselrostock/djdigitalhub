ac10feb59fa2b0e3e4a7af814249fc155d783f26
raselrostock <raselrostock@gmail.com>
2020-10-13 13:26:41 +0600

    :art: Add: Added error template

--- djdigitalhub/urls.py
+++ djdigitalhub/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls import handler400, handler403, handler404, handler500
 from django.views.generic import TemplateView
 
 urlpatterns = [
@@ -12,6 +12,12 @@ urlpatterns = [
     path('robots.txt', TemplateView.as_view(
         template_name="robots.txt", content_type='text/plain')),
 ]
+
+# Errors
+handler404 = 'djdigitalhub.views.not_found'
+handler500 = 'djdigitalhub.views.server_error'
+handler403 = 'djdigitalhub.views.permission_denied'
+handler400 = 'djdigitalhub.views.bad_request'
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
